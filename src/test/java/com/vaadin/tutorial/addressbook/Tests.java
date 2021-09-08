4815d27c5464249df1ccd96410f8ff946c331e3e
HariBabu Nalluri <harinvb@gmail.com>
2021-09-08 12:00:19 +0530

    Adding Tests to project

---
+++ src/test/java/com/vaadin/tutorial/addressbook/Tests.java
@@ -0,0 +1,23 @@
+package com.vaadin.tutorial.addressbook;
+
+
+import static org.junit.Assert.*;   
+import org.junit.Test;  
+
+public class Tests {  
+
+  @Test  
+  public void testFindMax(){  
+	  assertEquals(0, 0);
+  }  
+  
+  
+  @Test
+  public void test2() {
+	  assertEquals(true, true);
+  }
+  @Test
+  public void test3() {
+	  assertEquals("true", "true");
+  }
+}  
